8f08b48553e7424a5833243c403b2b3704293d20
yixueliang <yixueliang0302@gmail.com>
2026-02-01 11:03:37 +0100

    week 1

---
+++ Chapter1/Course/01_object.py
@@ -0,0 +1,17 @@
+#创建一个类
+#class 类名：
+#   属性
+#   行为
+
+class Student:
+    #属性（成员变量）：名字
+    name=None
+
+    #行为：成员方法
+
+#创建对象
+stu_1=Student()
+stu_2=Student()
+stu_1.name="Leo"
+stu_2.name="Laurent"
+print(stu_1.name)
---
+++ Chapter1/Course/02_student.py
@@ -0,0 +1,16 @@
+class Student:
+    #成员变量
+    name=None
+    age=None
+
+    #成员方法（函数在类里是一个方法
+    def say_hi(self):
+        print(f"bonjour je m appelle {self.name} et j ai {self.age} ans.")
+#创建对象
+stu_1=Student()
+stu_1.name="Alex"
+stu_1.age=18
+stu_1.say_hi()
+
+
+
